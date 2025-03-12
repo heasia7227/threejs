b94e8cc424b732b42159d6871f18f3f16ddb0c26
Arthur <108118358+heasia7227@users.noreply.github.com>
2025-03-12 16:53:02 +0800

    入门Three.js 第七课 旋转、缩放、平移几何体

---
+++ local-study/second-stage/js/5.js
@@ -0,0 +1,62 @@
+import * as THREE from "three";
+
+// 画布 Canvas 大小
+const width = 800;
+const height = 500;
+
+// 3D场景
+const scene = new THREE.Scene();
+
+//创建一个空的几何体对象
+const geometry = new THREE.BufferGeometry();
+//类型化数组创建顶点数据
+const vertices = new Float32Array([
+    0,
+    0,
+    0, //顶点1坐标, 4坐标
+    80,
+    0,
+    0, //顶点2坐标
+    80,
+    80,
+    0, //顶点3坐标, 5坐标
+    0,
+    80,
+    0, //顶点6坐标
+]);
+// 创建属性缓冲区对象
+//3个为一组，表示一个顶点的xyz坐标
+const attribue = new THREE.BufferAttribute(vertices, 3);
+// 设置几何体attributes属性的位置属性
+geometry.attributes.position = attribue;
+
+// Uint16Array类型数组创建顶点索引数据
+const indexes = new Uint16Array([
+    // 下面索引值对应顶点位置数据中的顶点坐标
+    0, 1, 2, 0, 2, 3,
+]);
+geometry.index = new THREE.BufferAttribute(indexes, 1);
+
+const material = new THREE.MeshBasicMaterial({
+    color: 0x00ffff,
+    side: THREE.FrontSide, //默认只有正面可见
+});
+// 创建线模型对象
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// 透视相机
+const camera = new THREE.PerspectiveCamera(30, width / height, 0.1, 3000);
+// 设置相机位置
+camera.position.set(200, 200, 200);
+// 设置相机观察点或对象
+camera.lookAt(0, 0, 0);
+
+// 渲染器
+const renderer = new THREE.WebGLRenderer();
+// 设置渲染器大小
+renderer.setSize(width, height);
+// 执行渲染
+renderer.render(scene, camera);
+// 把渲染结果显示到浏览器，结果是个canvas
+document.body.appendChild(renderer.domElement);
---
+++ local-study/second-stage/js/7.js
@@ -0,0 +1,112 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+// 画布 Canvas 大小
+const width = 800;
+const height = 500;
+
+// 3D场景
+const scene = new THREE.Scene();
+
+//创建一个空的几何体对象
+const geometry = new THREE.BufferGeometry();
+//类型化数组创建顶点数据
+const vertices = new Float32Array([
+    0,
+    0,
+    0, //顶点1坐标, 4坐标
+    80,
+    0,
+    0, //顶点2坐标
+    80,
+    80,
+    0, //顶点3坐标, 5坐标
+    0,
+    80,
+    0, //顶点6坐标
+]);
+// 创建属性缓冲区对象
+//3个为一组，表示一个顶点的xyz坐标
+const attribue = new THREE.BufferAttribute(vertices, 3);
+// 设置几何体attributes属性的位置属性
+geometry.attributes.position = attribue;
+
+// Uint16Array类型数组创建顶点索引数据
+const indexes = new Uint16Array([
+    // 下面索引值对应顶点位置数据中的顶点坐标
+    0, 1, 2, 0, 2, 3,
+]);
+geometry.index = new THREE.BufferAttribute(indexes, 1);
+// 矩形平面，有索引，两个三角形，有2个顶点重合，有4个顶点
+// 每个顶点的法线数据和顶点位置数据一一对应
+const normals = new Float32Array([
+    0,
+    0,
+    1, //顶点1法线( 法向量 )
+    0,
+    0,
+    1, //顶点2法线
+    0,
+    0,
+    1, //顶点3法线
+    0,
+    0,
+    1, //顶点4法线
+]);
+// 设置几何体的顶点法线属性.attributes.normal
+geometry.attributes.normal = new THREE.BufferAttribute(normals, 3);
+
+// 几何体xyz三个方向都放大2倍
+// geometry.scale(2, 2, 2);
+// 几何体沿着x轴平移50
+// geometry.translate(50, 0, 0);
+// 几何体绕着x轴旋转45度
+geometry.rotateX(Math.PI / 4);
+// 几何体旋转、缩放或平移之后，查看几何体顶点位置坐标的变化
+// BufferGeometry的旋转、缩放、平移等方法本质上就是改变顶点的位置坐标
+console.log("顶点位置数据", geometry.attributes.position);
+
+const material = new THREE.MeshLambertMaterial({
+    color: 0x00ffff,
+    side: THREE.DoubleSide, //默认只有正面可见
+});
+// 创建线模型对象
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// 环境光: 没有特定方向，整体改变场景的光照明暗
+const ambient = new THREE.AmbientLight(0xffffff, 0.4);
+// 把环境光加入到场景中
+scene.add(ambient);
+
+// 平行光
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+// 设置光源的方向：通过光源position属性和目标指向对象的position属性计算
+directionalLight.position.set(200, 100, 150);
+// 方向光指向对象网格模型mesh，可以不设置，默认的位置是0,0,0
+directionalLight.target = mesh;
+// 把平行光加入到场景中
+scene.add(directionalLight);
+
+// 透视相机
+const camera = new THREE.PerspectiveCamera(30, width / height, 0.1, 3000);
+// 设置相机位置
+camera.position.set(200, 200, 200);
+// 设置相机观察点或对象
+camera.lookAt(0, 0, 0);
+
+// 渲染器
+const renderer = new THREE.WebGLRenderer();
+// 设置渲染器大小
+renderer.setSize(width, height);
+// 执行渲染
+renderer.render(scene, camera);
+// 把渲染结果显示到浏览器，结果是个canvas
+document.body.appendChild(renderer.domElement);
+
+// 设置相机控件轨道控制器OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+// 如果OrbitControls改变了相机参数，重新调用渲染器渲染三维场景
+controls.addEventListener("change", function () {
+    renderer.render(scene, camera); //执行渲染操作
+});
