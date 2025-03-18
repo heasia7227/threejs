37a5e19dbd8c4b904f6da2eed8178dd72650c09f
Arthur <108118358+heasia7227@users.noreply.github.com>
2025-03-18 15:47:10 +0800

    进阶Three.js 第五课 美化地球

--- local-study/sixth-stage/js/5/earthModel.js
+++ local-study/sixth-stage/js/5/earthModel.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import {
     mix,
+    max,
+    step,
     vec3,
     vec4,
     texture,
@@ -11,12 +13,17 @@ import {
     normalWorld,
     positionWorld,
     cameraPosition,
+    bumpMap,
 } from "three/tsl";
 
 // 大气层白天颜色
 const atmosphereDayColor = uniform(color("#4db2ff"));
 // 大气层夜晚颜色（暮色）
 const atmosphereTwilightColor = uniform(color("#bc490b"));
+const roughnessLow = uniform(0.25);
+const roughnessHigh = uniform(0.35);
+// 贴图，加载器
+const textureLoader = new THREE.TextureLoader().setPath("./textures/");
 
 // 地球模型
 const earthModel = (sunModel) => {
@@ -27,8 +34,13 @@ const earthModel = (sunModel) => {
     // 在Three.js中，PMREM主要用于环境映射照明。使用标准的HDRI纹理时，可能会遇到周围的照明问题，导致阴影完全黑色。而使用PMREMGenerator可以解决这个问题
     // MeshStandardNodeMaterial 属于 PMREM
     const earthMaterial = new THREE.MeshStandardNodeMaterial();
-    earthMaterial.colorNode = getTextureDay(); // 白天的贴图
+
+    const bumpRoughnessClouds = getBumpRoughnessClouds();
+
+    earthMaterial.colorNode = getTextureDay(bumpRoughnessClouds.cloudsStrength); // 白天的贴图
     earthMaterial.outputNode = getTextureNight(sunModel); // 夜晚的贴图
+    earthMaterial.roughnessNode = getTextureRoughness(bumpRoughnessClouds); // 粗糙度的贴图
+    earthMaterial.normalNode = getTextureNormal(bumpRoughnessClouds); // 法线的贴图
 
     // 地球网格模型
     const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
@@ -75,24 +87,46 @@ const getAtmosphereColor = (sunModel) => {
     return atmosphereColor;
 };
 
-// 白天的贴图
-const getTextureDay = () => {
-    // 贴图，加载器
-    const textureLoader = new THREE.TextureLoader().setPath("./textures/");
-
+const getTextureDayJpg = () => {
     // 贴图, 白天
     const dayTexture = textureLoader.load("earth_day_4096.jpg");
     dayTexture.colorSpace = THREE.SRGBColorSpace; // 设置为SRGB颜色空间
     dayTexture.anisotropy = 8; // 数值越大Map越清晰，默认值1
 
-    // 贴图，云层
+    return dayTexture;
+};
+
+const getTextureNightJpg = () => {
+    const nightTexture = textureLoader.load("earth_night_4096.jpg");
+    nightTexture.colorSpace = THREE.SRGBColorSpace;
+    nightTexture.anisotropy = 8;
+
+    return nightTexture;
+};
+
+const getTextureBumpRoughnessCloudsJpg = () => {
     const bumpRoughnessCloudsTexture = textureLoader.load("earth_bump_roughness_clouds_4096.jpg");
     bumpRoughnessCloudsTexture.anisotropy = 8;
+
+    return bumpRoughnessCloudsTexture;
+};
+
+// 云层的影响力和贴图
+const getBumpRoughnessClouds = () => {
+    // 贴图，云层
+    const bumpRoughnessCloudsTexture = getTextureBumpRoughnessCloudsJpg();
 
     // uv 创建一个UV属性Node
     // texture: 创建一个texture node，返回TextureNode
     // smoothstep: 在两个值之间执行埃尔米特插值，返回Node
     const cloudsStrength = texture(bumpRoughnessCloudsTexture, uv()).b.smoothstep(0.2, 1);
+    return { cloudsStrength, bumpRoughnessCloudsTexture };
+};
+
+// 白天的贴图
+const getTextureDay = (cloudsStrength) => {
+    // 贴图, 白天
+    const dayTexture = getTextureDayJpg();
 
     // colorNode: 设置轮廓颜色，Node<vec3>类型
     // mix: 在两个值之间线性插值，返回Node
@@ -105,13 +139,8 @@ const getTextureDay = () => {
 
 // 夜晚的贴图
 const getTextureNight = (sunModel) => {
-    // 贴图，加载器
-    const textureLoader = new THREE.TextureLoader().setPath("./textures/");
-
     // 贴图，夜晚
-    const nightTexture = textureLoader.load("earth_night_4096.jpg");
-    nightTexture.colorSpace = THREE.SRGBColorSpace;
-    nightTexture.anisotropy = 8;
+    const nightTexture = getTextureNightJpg();
 
     // 大气层颜色
     const atmosphereColor = getAtmosphereColor(sunModel);
@@ -134,6 +163,27 @@ const getTextureNight = (sunModel) => {
     const outputNode = vec4(finalOutput, output.a);
 
     return outputNode;
+};
+
+// 粗糙度的贴图
+const getTextureRoughness = (bumpRoughnessClouds) => {
+    const roughness = max(
+        texture(bumpRoughnessClouds.bumpRoughnessCloudsTexture).g,
+        step(0.01, bumpRoughnessClouds.cloudsStrength)
+    );
+    const roughnessNode = roughness.remap(0, 1, roughnessLow, roughnessHigh);
+    return roughnessNode;
+};
+
+// 法线的贴图
+const getTextureNormal = (bumpRoughnessClouds) => {
+    const bumpElevation = max(
+        texture(bumpRoughnessClouds.bumpRoughnessCloudsTexture).r,
+        bumpRoughnessClouds.cloudsStrength
+    );
+    const normalNode = bumpMap(bumpElevation);
+
+    return normalNode;
 };
 
 export { earthModel, atmosphereModel };
